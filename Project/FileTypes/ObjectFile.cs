89a30e984278c48053aedb867ccb3004a966004f
jake1029 <55038561+jake1029@users.noreply.github.com>
2022-01-27 23:59:50 -0500

    Re-pushing with correct gitignore.

---
+++ Project/FileTypes/ObjectFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMHEditor.Project.FileTypes
+{
+    enum ObjectAttributeType
+    {
+        INT, FLOAT, STRINGID, STRING
+    }
+    class ObjectAttribute
+    {
+        string name;
+        public ObjectAttributeType type;
+        object value;
+    }
+    class ObjectFile
+    {
+        List<ObjectAttribute> attributes;
+    }
+}
