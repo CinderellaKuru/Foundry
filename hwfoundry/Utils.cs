57dd0eb337cc46af71eccc49b2c0fc59f8c2d408
stumpy1029 <55038561+stumpy1029@users.noreply.github.com>
2022-07-15 14:32:40 -0400

    Removed Jitter references.

--- hwfoundry/Utils.cs
+++ hwfoundry/Utils.cs
@@ -6,19 +6,11 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Drawing;
 using OpenTK;
-using Jitter.LinearMath;
 
 namespace Foundry
 {
     static class Convert
     {
-        public static Vector3 ToTKVec3(JVector jVector)
-        {
-            return new Vector3(
-                jVector.X,
-                jVector.Y,
-                jVector.Z );
-        }
         public static Vector3 ToTKVec3(BEPUutilities.Vector3 vector)
         {
             return new Vector3(
@@ -29,13 +21,6 @@ namespace Foundry
         public static BEPUutilities.Vector3 ToBepuVec3(OpenTK.Vector3 v)
         {
             return new BEPUutilities.Vector3(v.X, v.Y, v.Z);
-        }
-        public static JVector ToJVec3(Vector3 tkVector)
-        {
-            return new JVector(
-                tkVector.X,
-                tkVector.Y,
-                tkVector.Z );
         }
         public static List<float> GetFloats(this List<Vector3> v)
         {
@@ -52,18 +37,6 @@ namespace Foundry
 
     static class Util
     {
-        public static float Distance(JVector a, JVector b)
-        {
-            return Vector3.Distance(Convert.ToTKVec3(a), Convert.ToTKVec3(b));
-        }
-        public static bool DrawingPointIsInsideControl(System.Drawing.Point p, System.Windows.Forms.Control c)
-        {
-            if (p.X < c.PointToScreen(new Point(0, 0)).X ||
-                p.X > c.PointToScreen(new Point(0, 0)).X + c.Width ||
-                p.Y < c.PointToScreen(new Point(0, 0)).Y ||
-                p.Y > c.PointToScreen(new Point(0, 0)).Y + c.Height) return false;
-            else return true;
-        }
         public static uint CalcAdler32(byte[] barr, int offs, int len)
         {
             const int mod = 65521;
